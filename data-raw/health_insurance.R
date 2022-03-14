97a867e8bbadc5bc909e1fde30f8af2a50806952
Kathryn Linehan <kjl5t@virginia.edu>
2022-03-14 15:53:46 -0400

    fixed health insurance data, created package documentation

--- data-raw/health_insurance.R
+++ data-raw/health_insurance.R
@@ -8,42 +8,11 @@ library(tidyverse)
 
 census_api_key(Sys.getenv("CENSUS_API_KEY"))
 
-
-# VA data -----------------------------------------------------
-
-con <- get_db_conn()
-va_df <- DBI::dbReadTable(con, c("dc_health_behavior_diet",
-                                 "va_hdcttr_acs5_2015_2019_no_health_insurance_19_to_64"))
-hd <- DBI::dbReadTable(con, c("dc_geographies", "va_hd_vdh_2021_health_district_geo_names"))
-
-# match with new health district geoids
-
-new_va_df <- merge(va_df, hd[ , c(1,3)], by = "region_name", all.x = TRUE)
-new_va_df$geoid <- ifelse(is.na(new_va_df$geoid.y), new_va_df$geoid.x, new_va_df$geoid.y)
-
-no_hlth_ins_df <- new_va_df %>%
-  select(geoid, region_type, region_name, year, measure, value, measure_type)
-
-# add health insurance pct as a measure
-
-hlth_ins_df <- no_hlth_ins_df
-hlth_ins_df$measure <- "hlth_ins_pct"
-hlth_ins_df$value = 100 - hlth_ins_df$value
-
-df <- rbind(no_hlth_ins_df, hlth_ins_df)
-
-dc_dbWriteTable(con, "dc_health_behavior_diet",
-                "va_hdcttr_acs5_2015_2019_no_health_insurance_19_to_64", df)
-DBI::dbDisconnect(con)
-
-rm(con,df,hd,hlth_ins_df,new_va_df,no_hlth_ins_df,va_df)
-
-
-# NCR data pull -----------------------------------------
+# DC, MD, VA data pull -----------------------------------------
 
 states <- c("VA", "MD", "DC")
 geographies <- c("county", "tract", "block group")
-years <- c(2015:2019)  # no block group data -- table B27010 has block gp data
+years <- c(2015:2019)
 
 ins <- NULL
 ins_wide <- NULL
@@ -55,7 +24,7 @@ for(state in states)
     for(geography in geographies)
     {
       # pull health insurance vars
-      vars = c("B18135_013", "C27001I_007")
+      vars = c("B27010_033", "B27010_050", "B27010_018", "B27010_034")
 
       ins <- get_acs(geography = geography, variables = vars, state = state, year = year,
                      geometry = FALSE, survey = "acs5", cache_table = TRUE, output = "wide") %>%
@@ -64,7 +33,7 @@ for(state in states)
           region_type = as.character(geography),
           region_name = NAME,
           year = year,
-          no_hlth_ins_pct = round((C27001I_007E/B18135_013E)*100, 2),
+          no_hlth_ins_pct = 100 * (B27010_033E + B27010_050E)/(B27010_018E + B27010_034E),
           hlth_ins_pct = 100 - no_hlth_ins_pct
         ) %>%
         arrange(geoid)
@@ -80,16 +49,52 @@ ins_long <- gather(ins_wide, measure, value, no_hlth_ins_pct:hlth_ins_pct)
 ins_long$measure_type <- "percent"
 
 
-# NCR ------------------------------------------
+# filter to NCR and VA ------------------------------------------
 
 ncr_counties <- c("^24021|^24031|^24033|^24017|^11001|^51107|^51059|^51153|^51013|^51510|^51683|^51600|^51610|^51685")
 
-# filter to NCR
-
 ncr_ins <- ins_long %>% dplyr::filter(str_detect(geoid, ncr_counties))
+va_ins <- ins_long %>% dplyr::filter(str_detect(geoid, "^51"))
+
+
+# aggregate VA data to health districts ---------------------------
+
+con <- get_db_conn()
+hd_cts <- DBI::dbReadTable(con, c("dc_health_behavior_diet", "va_hd_vhd_2021_virginia_health_districts"))
+hd <- DBI::dbReadTable(con, c("dc_geographies", "va_hd_vdh_2021_health_district_geo_names"))
+DBI::dbDisconnect(con)
+
+hd_cts$geometry <- NULL
+
+va_ct_ins <- va_ins %>%
+  filter(region_type == "county")
+
+# add health district column
+
+va_ct_ins_whd <- merge(va_ct_ins, hd_cts[ , c(1,3)], by.x = "geoid", by.y = "geoid_county", all.x = TRUE)
+
+va_hd_ins <- va_ct_ins_whd %>%
+  group_by(health_district, year, measure) %>%
+  summarise(value = mean(value)) %>%
+  rename(region_name = health_district)
+
+# add new hd geoids
+
+new_va_hd_ins <- merge(va_hd_ins, hd, by = "region_name", all.x = TRUE)
+new_va_hd_ins$measure_type = "percent"
+new_va_hd_ins <- new_va_hd_ins[ , c(5,6,1,2,3,4,7)]
+
+va_cttr_ins <- va_ins %>%
+  filter(region_type == "county" | region_type == "tract")
+va_hdcttr_ins <- rbind(va_cttr_ins, new_va_hd_ins)
+
+
+# write to database ---------------------
 
 con <- get_db_conn()
 dc_dbWriteTable(con, "dc_health_behavior_diet",
-                "ncr_cttrbg_acs5_2015_2019_no_health_insurance_19_to_64", ins_long)
+                "ncr_cttrbg_acs5_2015_2019_no_health_insurance_19_to_64", ncr_ins)
+dc_dbWriteTable(con, "dc_health_behavior_diet",
+                "va_hdcttr_acs5_2015_2019_no_health_insurance_19_to_64", va_hdcttr_ins)
 DBI::dbDisconnect(con)
 
